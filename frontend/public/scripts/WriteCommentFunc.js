329cb7e7b890cdb4f48ecf00183a5565d4c9be24
Venisimo <ledokol522@gmail.com>
2024-03-27 23:45:28 +0700

    finalized comments

--- frontend/public/scripts/WriteCommentFunc.js
+++ frontend/public/scripts/WriteCommentFunc.js
@@ -10,7 +10,13 @@ CommentText.addEventListener('keydown', function (event) {
 });
 
 function resizeTextarea() {
-    CommentText.style.height = CommentText.scrollHeight + 'px'; 
+    CommentText.style.height = '20px'; 
+    CommentText.style.height = CommentText.scrollHeight + 'px';
 }
 
-window.addEventListener('load', resizeTextarea);
+CommentText.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        console.log('send');
+        console.log(CommentText.value);
+    }
+});
