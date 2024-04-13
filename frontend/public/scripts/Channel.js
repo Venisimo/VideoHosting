e3ac5e7a6c5815926bd33d215a8d68606976a08e
Venisimo <ledokol522@gmail.com>
2024-04-13 23:48:19 +0700

    add lower panel in Channel.html

---
+++ frontend/public/scripts/Channel.js
@@ -0,0 +1,9 @@
+ListChannelVideos.addEventListener('click', function() {
+    LineChoiseChannel.style.marginLeft = "680px";
+});
+ListChannelSubscriptions.addEventListener('click', function() {
+    LineChoiseChannel.style.marginLeft = "890px";
+});
+ListChannelAbout.addEventListener('click', function() {
+    LineChoiseChannel.style.marginLeft = "1125px";
+});
