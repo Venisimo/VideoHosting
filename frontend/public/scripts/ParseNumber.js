aeb49d76290562b4c1f0ff1cf43213c2d4d5cfba
Venisimo <ledokol522@gmail.com>
2024-04-16 21:57:54 +0700

    fix ParseNumber

--- frontend/public/scripts/ParseNumber.js
+++ frontend/public/scripts/ParseNumber.js
@@ -1,32 +1,19 @@
 function ParseNumber() {
     Num.forEach(element => {
-        if (Number(element.innerHTML) < 1000) {
+        if (element.innerHTML.length == 3) {
             element.style.marginRight = "0px";
-        } else if (Number(element.innerHTML) > 999 && Number(element.innerHTML) < 1100) {
-            element.innerHTML = element.innerHTML[0];
-        } else if (Number(element.innerHTML) > 1099 && Number(element.innerHTML) < 10000) {
-            element.innerHTML = element.innerHTML[0] + ',' + element.innerHTML[1];
-        } else if (Number(element.innerHTML) > 9999 && Number(element.innerHTML) < 100000) {
+        } else if (element.innerHTML.length == 4 || element.innerHTML.length == 7 || element.innerHTML.length == 10) {
+            if (element.innerHTML[1] != 0) {
+                element.innerHTML = element.innerHTML[0] + ',' + element.innerHTML[1];
+            } else {
+                element.innerHTML = element.innerHTML[0];
+            }
+        } else if (element.innerHTML.length == 5 || element.innerHTML.length == 8 || element.innerHTML.length == 11) {
             element.innerHTML = element.innerHTML[0] + element.innerHTML[1];
-        } else if (Number(element.innerHTML) > 99999 && Number(element.innerHTML) < 1000000) {
-            element.innerHTML = element.innerHTML[0] + element.innerHTML[1] + element.innerHTML[2];
-        } else if (Number(element.innerHTML) > 999999 && Number(element.innerHTML) < 1100000) {
-            element.innerHTML = element.innerHTML[0];
-        } else if (Number(element.innerHTML) > 1099999 && Number(element.innerHTML) < 10000000) {
-            element.innerHTML = element.innerHTML[0] + ',' + element.innerHTML[1];
-        } else if (Number(element.innerHTML) > 9999999 && Number(element.innerHTML) < 100000000) {
-            element.innerHTML = element.innerHTML[0] + element.innerHTML[1];             
-        } else if (Number(element.innerHTML) > 99999999 && Number(element.innerHTML) < 1000000000) {
-            element.innerHTML = element.innerHTML[0] + element.innerHTML[1] + element.innerHTML[2];
-        } else if (Number(element.innerHTML) > 999999999 && Number(element.innerHTML) < 1100000000) {
-            element.innerHTML = element.innerHTML[0]; 
-        } else if (Number(element.innerHTML) > 1099999999 && Number(element.innerHTML) < 10000000000) {
-            element.innerHTML = element.innerHTML[0] + ',' + element.innerHTML[1];
-        } else if (Number(element.innerHTML) > 1099999999 && Number(element.innerHTML) < 100000000000) {
-            element.innerHTML = element.innerHTML[0] + element.innerHTML[1];
-        } else if (Number(element.innerHTML) > 9999999999 && Number(element.innerHTML) < 1000000000000) {
-            element.innerHTML = element.innerHTML[0] + element.innerHTML[1] + element.innerHTML[2];
+        } else if (element.innerHTML.length == 6 || element.innerHTML.length == 9 || element.innerHTML.length == 12) {
+            element.innerHTML = element.innerHTML[0] + element.innerHTML[1] + element.innerHTML[3];
         }
     })  
 }
 ParseNumber();
+// ListChannelVideos.addEventListener('click', ParseNumber)
